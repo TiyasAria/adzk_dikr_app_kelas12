66b12a1a068ed099b0204109f518d419701cd9cd
Tyas Aria Pratiwi <tiyasaria123@gmail.com>
2021-09-20 07:37:10 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/din/tiyas/adzk_dikr_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.din.tiyas.adzk_dikr_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/dzikirSetiapSaat/dzikir_setiap_saat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DzikirSetiapSaat extends StatelessWidget {
+  const DzikirSetiapSaat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
