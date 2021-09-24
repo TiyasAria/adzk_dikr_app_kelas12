eaad3bff1a42b470c5d85ba9a3bb95f775469569
Tyas Aria Pratiwi <tiyasaria123@gmail.com>
2021-09-24 08:25:54 +0700

    new commit

---
+++ lib/ui/harianPage/detail_screen_doa.dart
@@ -0,0 +1,13 @@
+import 'package:adzk_dikr_app/model/model_doa.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreenDoa extends StatelessWidget {
+  const DetailScreenDoa({Key? key, required this.doa}) : super(key: key);
+
+  final ModelDoa doa;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
